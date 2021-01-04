3937fb811d032e98c4c511eb67dc633949889827
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2021-01-04 15:15:45 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import puppeteer from 'puppeteer'
-
-import createApp from './app.js'
-
-const app = createApp(express, puppeteer)
-
-app.listen(process.env.PORT || 3000)
